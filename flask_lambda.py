b1a49a88fa7e69c8c92e4c9cdba968193b086179
zrq495 <zrq495@gmail.com>
2018-12-24 14:32:59 +0800

    py3 compat

--- flask_lambda.py
+++ flask_lambda.py
@@ -15,6 +15,8 @@
 #    under the License.
 
 import sys
+import datetime
+from decimal import Decimal
 
 try:
     from urllib import urlencode
@@ -31,10 +33,97 @@ except ImportError:
     except ImportError:
         from io import StringIO
 
+import six
 from werkzeug.wrappers import BaseRequest
 
 
 __version__ = '0.0.4'
+
+
+class Promise(object):
+    """
+    This is just a base class for the proxy class created in
+    the closure of the lazy function. It can be used to recognize
+    promises in code.
+    """
+    pass
+
+
+class _UnicodeDecodeError(UnicodeDecodeError):
+    def __init__(self, obj, *args):
+        self.obj = obj
+        UnicodeDecodeError.__init__(self, *args)
+
+    def __str__(self):
+        original = UnicodeDecodeError.__str__(self)
+        return '%s. You passed in %r (%s)' % (original, self.obj,
+                                              type(self.obj))
+
+
+_PROTECTED_TYPES = six.integer_types + (type(None), float, Decimal,
+                                        datetime.datetime, datetime.date,
+                                        datetime.time)
+
+
+def is_protected_type(obj):
+    """Determine if the object instance is of a protected type.
+    Objects of protected types are preserved as-is when passed to
+    force_text(strings_only=True).
+    """
+    return isinstance(obj, _PROTECTED_TYPES)
+
+
+def smart_text(s, encoding='utf-8', strings_only=False, errors='strict'):
+    """
+    Returns a text object representing 's' -- unicode on Python 2 and str on
+    Python 3. Treats bytestrings using the 'encoding' codec.
+    If strings_only is True, don't convert (some) non-string-like objects.
+    """
+    if isinstance(s, Promise):
+        # The input is the result of a gettext_lazy() call.
+        return s
+    return force_text(s, encoding, strings_only, errors)
+
+
+def force_text(s, encoding='utf-8', strings_only=False, errors='strict'):
+    """
+    Similar to smart_text, except that lazy instances are resolved to
+    strings, rather than kept as lazy objects.
+    If strings_only is True, don't convert (some) non-string-like objects.
+    """
+    # Handle the common case first for performance reasons.
+    if issubclass(type(s), six.text_type):
+        return s
+    if strings_only and is_protected_type(s):
+        return s
+    try:
+        if not issubclass(type(s), six.string_types):
+            if six.PY3:
+                if isinstance(s, bytes):
+                    s = six.text_type(s, encoding, errors)
+                else:
+                    s = six.text_type(s)
+            elif hasattr(s, '__unicode__'):
+                s = six.text_type(s)
+            else:
+                s = six.text_type(bytes(s), encoding, errors)
+        else:
+            # Note: We use .decode() here, instead of six.text_type(s, encoding,
+            # errors), so that if s is a SafeBytes, it ends up being a
+            # SafeText at the end.
+            s = s.decode(encoding, errors)
+    except UnicodeDecodeError as e:
+        if not isinstance(s, Exception):
+            raise _UnicodeDecodeError(s, *e.args)
+        else:
+            # If we get to here, the caller has passed in an Exception
+            # subclass populated with non-ASCII bytestring data without a
+            # working unicode method. Try to handle this without raising a
+            # further exception by individually forcing the exception args
+            # to unicode.
+            s = ' '.join(force_text(arg, encoding, strings_only, errors)
+                         for arg in s)
+    return s
 
 
 def make_environ(event):
@@ -62,8 +151,7 @@ def make_environ(event):
     environ['SERVER_PROTOCOL'] = 'HTTP/1.1'
 
     body = event.get('body', '')
-    if body:
-        body = body.encode('utf-8')
+    body = smart_text(body)
     environ['CONTENT_LENGTH'] = str(
         len(body) if body else ''
     )
